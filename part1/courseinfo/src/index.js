bba44d4d2b138ae3666e91761389eaf86b8d547a
Jose De La Cruz <jldlcv@gmail.com>
2022-04-13 22:13:57 -0500

    updated to version 18

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,7 +1,6 @@
-import ReactDOM from 'react-dom';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />, );
