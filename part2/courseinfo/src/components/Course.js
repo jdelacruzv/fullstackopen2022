b972f6e0db38c0e139cf2c6f52d34b63b8ced521
Jose De La Cruz <jldlcv@gmail.com>
2022-05-11 10:55:47 -0500

    exercise 2.4 courseinfo step9

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,12 +2,17 @@ import Content from "./Content";
 import Header from "./Header";
 import Total from "./Total";
 
-const Course = (props) => {	
+const Course = ({ courses }) => {
 	return (
 		<>
-			<Header name={props.course.name} />
-			<Content parts={props.course.parts} />
-			<Total parts={props.course.parts} />
+			<h1>Web development curriculum</h1>
+			{courses.map((course) => (
+				<div key={course.id}>
+					<Header name={course.name} />
+					<Content parts={course.parts} />
+					<Total parts={course.parts} />
+				</div>
+			))}
 		</>
 	);
 };
