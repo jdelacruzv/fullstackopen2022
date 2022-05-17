5358560df2000fca7faf2ea08f6a28d86d39fb64
Jose De La Cruz <jldlcv@gmail.com>
2022-05-17 18:13:35 -0500

    exercise 2.7 phonebook step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,44 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
-  ]);
-  const [newName, setNewName] = useState("");
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [newName, setNewName] = useState("");
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
 
-  const addName = (event) => {
-    event.preventDefault();
-    const nameObject = {
-      name: newName
-    };
-    setPersons(persons.concat(nameObject));
-    setNewName("");
-  };
+  const nameObject = {
+		name: newName,
+	};
 
+	const searchName = () => {
+    persons.find(person => person.name === newName)
+      ? alert(`${newName} is already added to phonebook`)
+      : setPersons(persons.concat(nameObject));
+  }; 
+
+	const addName = (event) => {
+		event.preventDefault();
+    searchName();
+		setNewName("");
+	};
+	
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          Name: 
-          <input 
-            type="text" 
-            value={newName} 
-            onChange={handleNameChange} 
-          />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name}</div>)}
-    </div>
-  );
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addName}>
+				<div>
+					Name:
+					<input type="text" value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<div key={person.name}>{person.name}</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
